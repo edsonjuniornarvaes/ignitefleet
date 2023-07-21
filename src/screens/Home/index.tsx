8c98fb0d4e2015ff5ded3e0648f93163931335b6
Edson Junior de Andrade Narvaes <edsonjunior.narvaes@gmail.com>
2023-07-21 09:12:25 -0300

    feat: implements navigation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,24 @@
-import { Container } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+
+import { Container, Content } from "./styles";
 
 import { HomeHeader } from "../../components/HomeHeader";
+import { CarStatus } from "../../components/CarStatus";
 
 export function Home() {
+  const { navigate } = useNavigation();
+
+  function handleRegisterMoviment() {
+    navigate("departure");
+  }
+
   return (
     <Container>
       <HomeHeader />
+
+      <Content>
+        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMoviment} />
+      </Content>
     </Container>
   );
 }
