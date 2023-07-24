a376b6d85535a885c82629bc0d030b555261a4f0
Edson Junior de Andrade Narvaes <edsonjunior.narvaes@gmail.com>
2023-07-24 09:23:32 -0300

    feat: Creating license plate entry

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,10 +1,15 @@
-import { Header } from '../../components/Header';
-import { Container } from './styles';
+import { Header } from "../../components/Header";
+import { LicensePlateInput } from "../../components/LicensePlateInput";
+import { Container, Content } from "./styles";
 
 export function Departure() {
   return (
     <Container>
-      <Header title='Saída' />
+      <Header title="Saída" />
+
+      <Content>
+        <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+      </Content>
     </Container>
   );
 }
