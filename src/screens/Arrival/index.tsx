31f07244a9c7e7b3e386b47234a277d65b94e606
Edson Junior de Andrade Narvaes <edsonjunior.narvaes@gmail.com>
2023-07-27 09:07:53 -0300

    feat: Creating the check-in screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,6 +1,16 @@
 import { useRoute } from "@react-navigation/native";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from "./styles";
+
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
 
 type RouteParamProps = {
   id: string;
@@ -11,7 +21,27 @@ export function Arrival() {
 
   const { id } = route.params as RouteParamProps;
 
-  console.log(id);
+  return (
+    <Container>
+      <Header title="Chegada" />
+      <Content>
+        <Label>Placa do veículo</Label>
 
-  return <Container></Container>;
+        <LicensePlate>XXX0000</LicensePlate>
+
+        <Label>Finalidade</Label>
+
+        <Description>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+          voluptate atque necessitatibus voluptatibus eveniet rerum maiores
+          neque laborum obcaecati eos debitis deleniti tempore veritatis,
+          voluptates modi, optio ullam quasi dolor!
+        </Description>
+
+        <Footer>
+          <Button title="Registrar chegada" />
+        </Footer>
+      </Content>
+    </Container>
+  );
 }
