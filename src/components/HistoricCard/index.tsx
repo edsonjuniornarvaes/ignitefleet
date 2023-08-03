aab8016a53e53a5854d3052c70090eca4ee2ad19
Edson Junior <edsonjunior.narvaes@gmail.com>
2023-08-03 09:13:13 -0300

    feat: displaying vehicle details in use

--- src/components/HistoricCard/index.tsx
+++ src/components/HistoricCard/index.tsx
@@ -18,13 +18,12 @@ export function HistoricCard({ data, ...rest }: Props) {
   const { COLORS } = useTheme();
 
   return (
-    <Container {...rest}>
+    <Container activeOpacity={0.7} {...rest}>
       <Info>
         <LicensePlate>{data.licensePlate}</LicensePlate>
 
         <Departure>{data.created}</Departure>
       </Info>
-
       {data.isSync ? (
         <Check size={24} color={COLORS.BRAND_LIGHT} />
       ) : (
